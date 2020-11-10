71ee9c3dda1f3d9f5984b031a99a1b8c82b89d94
Nonhlanhla Khumalo <Khumalononhlanhla58@gmail.com>
2020-11-10 15:21:50 +0200

    added solution to task 3

---
+++ solutiontask3.js
@@ -0,0 +1,10 @@
+function Task3(num1, num2) {
+    let result;
+    if(num1 == 65|| num2 == 65 || num1 + num2 == 65) 
+    {result = true}
+    else {result = false}
+    return result
+
+}
+    
+console.log(Task3())
