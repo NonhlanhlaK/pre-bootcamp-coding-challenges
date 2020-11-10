e4bdd28b8d6a762e2c88af0ac20a83d5ce7cfcaa
Nonhlanhla Khumalo <Khumalononhlanhla58@gmail.com>
2020-11-10 15:38:19 +0200

    added solution to task 4

---
+++ solutiontask4.js
@@ -0,0 +1,12 @@
+function Task4(num1, num2) {
+let result 
+var contains3 = [num1 + num2]  
+if(contains3.includes(3)||num1 == 3|| num2 ==3)  {
+    result = true;
+} else {
+    result = false;
+}
+return result;
+}
+
+console.log(Task4())
