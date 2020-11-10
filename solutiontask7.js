dfd12622d364ce554a02bbe3e9866af7ceada96f
Nonhlanhla Khumalo <Khumalononhlanhla58@gmail.com>
2020-11-10 16:20:57 +0200

    added solution to task 7

---
+++ solutiontask7.js
@@ -0,0 +1,16 @@
+function Task7(num1) {
+    var temperature = 'f'
+    return num1 * 33.8 + temperature
+    
+}
+
+ function Task7_2(num1) {
+        var temperature = 'c'
+        return num1 / 33.8 + temperature
+    }
+
+//this part executes function task7
+console.log(Task7())
+
+//this part executes function task 7_2
+console.log(Task7_2())
