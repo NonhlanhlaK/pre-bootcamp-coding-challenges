016e0d515a47ebbfab47cb05444c48a8910b5453
Nonhlanhla Khumalo <Khumalononhlanhla58@gmail.com>
2020-11-13 16:23:44 +0200

    added solution to task 10

---
+++ solutiontask10.js
@@ -0,0 +1,10 @@
+function printVowels(word) {
+    var result = '';
+    var vowelMatches = word.match(/[aeiou]/gi);
+    for (a = 0; a < vowelMatches.length; a++) {
+        result += vowelMatches[a];
+    }
+    return result
+}
+
+console.log(printVowels())
