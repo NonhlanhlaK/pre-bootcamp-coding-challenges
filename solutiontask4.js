795144b661f576816b32797219c2159acf9fd1ad
Nonhlanhla Khumalo <Khumalononhlanhla58@gmail.com>
2020-11-15 12:24:33 +0200

    bug removed

--- solutiontask4.js
+++ solutiontask4.js
@@ -1,15 +1,11 @@
 function isOrContains3(num1, num2) {
 
   var num = num1 + num2
-   if (num.toString().match(/3/))
+   if (num.toString().match(/3/) && (num1 == 3 || num2 == 3))
    return true
    else return false
-   if (true && num1 == 3|| num2 ==3)
-      return true;
-    else 
-      return false;
    
   
   }
   
-  console.log(isOrContains3())
+  console.log(isOrContains3(3, 11))
