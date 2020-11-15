eaeb97f2665483e47046cfde1368b01e0748569f
Nonhlanhla Khumalo <Khumalononhlanhla58@gmail.com>
2020-11-15 09:29:39 +0200

    removed Task 4 bug

--- solutiontask4.js
+++ solutiontask4.js
@@ -1,14 +1,15 @@
 function isOrContains3(num1, num2) {
 
-    var num = num1 + num2
-     if (num.toString().match(/3/))
-     return true
-     if (num1 == 3|| num2 ==3)
-        return true;
-      else 
-        return false;
-     
-    
-    }
-    
-    console.log(isOrContains3())
+  var num = num1 + num2
+   if (num.toString().match(/3/))
+   return true
+   else return false
+   if (true && num1 == 3|| num2 ==3)
+      return true;
+    else 
+      return false;
+   
+  
+  }
+  
+  console.log(isOrContains3())
